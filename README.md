887a4282426fe767dd61fe4a8d9632b814fe1cb2
Ogün Baysal <170101035@ogrenci.yalova.edu.tr>
2019-01-01 21:01:22 +0300

    Hyphenation With C

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# hyphenation
-Hyphenation on C
+# Hyphenation
+## It is a basic hyphenation app written with C for Turkish language.
+
+### Function : 
+#### It finds the hyphenated value of given word by user. It just contains 4 rule which i have taken from the [TDK's Website](http://www.tdk.gov.tr/index.php?id=208:Hece..&option=com_content).
+
+### -------------------------------------
+
+# Heceleme
+## Türkçe dili için c ile yazılmış basit bir heceleme uygulaması.
+	
+### Fonksiyon : 
+#### Kullanıcının girdiği bir kelimenin hecelenmiş versiyonunu bulur. Sadece TDK'nın Websitesinden alınmış 4 kuralı içermektedir.
--- hyphenation.c
+++ hyphenation.c
@@ -1,43 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// GROUP INFORMATION
-// 1. Student : 
-//     OGÜN BAYSAL 170101035
-// 2. Student :
-//     MELİH ARIKAN 170101069
-
-
-//bu fonksyion eger harf sesli ise 1 degil ise 0 dondurur
 int isVowel(char);
 
 int main()
 {
-    //word kullanicinin girecegi kelime
-    //wordHype hecelenmis hali olacak
-    //buraya ihtiyaciniz olan diger degiskenleri tanimlayabilirsiniz.
     char word [50] = "", wordHyp [100] = "";
-    //asagidaki iki satiri degistirmeyiniz.
     printf("Enter a word for hyphenation:\n");
     scanf("%s", word);
-
-
-    //yazacaginiz kod buraya gelsin. Ancak printf ve scanf gibi
-    //ekranda gorunecek ciktilari degistirecek hic bir fonksyion kullanmayiniz.
-    //char dizisi (string) ile calistigimiz icin word degiskenini bir fonksyiona
-    //gonderip onun ustunde heceleme yapabilirsiniz ancak su an icin pointerlari
-    //ogrenmedigimiz icin bu    oldukca zor olacaktir.
-    //ancak metin icindeki bir harf sesli mi degil mi bunu ogrenmek icin gereken
-    //fonksyionu size tanimladik.
-    //sadeece Ingiliz alfabesindeki karakterler kullanilacak. Turkce' ye ait
-    //ö, ı, ü, ç, ş ... gibi harflere dikkat etmenize gerek yoktur.
-    //Size strcat ve strncat fonksyionlar cok yardimci olacaktir.
-    //lutfen kullanimlarina bakiniz.
-    //Kullanici okul gibi bir kelime girdiyse
-    //hecelerine - karakteriyle ayrilmalidir.
-    //o-kul gibi. Bu heceleme islemini yaparken son karakterde - isareti kalmamalidir
-    //yani o-kul- olmamalidir.
-
 
     int j=0;
     int i = 0;
@@ -68,13 +37,6 @@ int main()
         i++;
     }
     wordHyp[i+j] = '\0';
-    
-    
-    
-      
-   
-	//kodunuz asagidaki satirlardan once bitmeli
-    //asagidaki iki satiri degistirmeyiniz
     printf("%s\n", wordHyp);
     return 0;
 }
